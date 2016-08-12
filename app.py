174abe31c22e5b1ed56cacda2b7c35e935db66bd
Jamie Cox <jamie@potatolondon.com>
2016-08-12 21:47:35 +0100

    warmup

--- app.py
+++ app.py
@@ -15,38 +15,49 @@ app.cloud = heroku.from_key(os.environ['HEROKU_KEY'])
 app.heroku_app = app.cloud.apps[os.environ['HEROKU_APP']]
 
 
-def get_template(url):
-    key = 'html-' + url.split('/')[-1]
-    html = app.cache.get(key)
-    if not html:
+def get_template(url, prefix='html'):
+    key = prefix + '-' + url.split('/')[-1]
+    template = app.cache.get(key)
+    if not template:
         response = requests.get(url)
         if response.ok:
-            html = response.content
-            app.cache.set(key, html, 60 * 10)
+            template = response.content
+            app.cache.set(key, template, 60 * 10)
         else:
             return ''
-    return html
+    return template
 
 
 @app.route('/', methods=['GET'])
 @app.cache.cached(timeout=60 * 60)
 def index():
-    template = get_template(index.template_url)
+    template = get_template(index.html_url)
     return flask.render_template_string(template), 200
-index.template_url = os.environ['INDEX_TEMPLATE_URL']
+index.html_url = os.environ['INDEX_HTML_URL']
 
 
 @app.route('/set', methods=['GET'])
 def set_url():
-    if flask.request.args.get('key', '') == app.config['SECRET_KEY']:
-        route = flask.request.args.get('route')
-        url = flask.request.args.get('url')
+    params = flask.request.args
+    if params.get('key', '') == app.config['SECRET_KEY']:
+        route = params.get('route')
+        url = params.get('url')
+        suffix = '_{}_URL'.format(params.get('type', 'html'))
         if route and url:
-            route = (route + '_TEMPLATE_URL').upper()
-            app.heroku_app.config[route] = url
+            template_key = (route + suffix).upper()
+            app.heroku_app.config[template_key] = url
             return 'done', 200
     return 'denied', 200
 
 
+def warmup():
+    for key, value in os.environ.items():
+        if key.endswith('HTML_URL'):
+            get_template(value)
+        elif key.endswith('CSS_URL'):
+            get_template(value, 'css')
+
+
 if __name__ == "__main__":
+    warmup()
     app.run(debug=app.config.get('DEBUG', True))
